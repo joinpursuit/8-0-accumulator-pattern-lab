19c986dc23fd235915841bb4fe025fdc6c9f8589
ERIAN CABALLERO <eriancaballero@ERIANs-MBP.home>
2021-07-21 23:56:20 -0400

    Accumulator Lab

--- index.js
+++ index.js
@@ -3,7 +3,22 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  let nums = false;
+  //let emptyArr;
+  if( rolls.length === 0){
+    nums = true;
+  }
+  for(let i=0;i<rolls.length;i++){
+      let rollsArr = rolls[i];
+       if(typeof rollsArr === "number"){
+        nums = true
+      } else if(typeof rollsArr !== "number" ) {
+        nums = false;
+      }
+  }
+  return nums;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +26,23 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  // let valueFound = false;
+  if (rolls.length === 0){
+  return null;
+}
+  for(let i = 0; i < rolls.length;i++){
+      let rollsInArr = rolls[i];
+      if(rollsInArr === value){
+        return value;
+      // }else if ( rollsInArr === null){
+      //   return null;
+      }
+   
+  }
+  
+}
+
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,15 +50,32 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+let newArr =[];
+if (rolls.length === 0){
+  return [];
+}
+  for(let i =0 ; i >= rolls.length;i++){
+      let newRoll = rolls[i];
+      if(newRoll >= lowest){
 
+      }
+      
+  }
+}
+// console.log(newArr)
+// returns newArr;
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
-
+function getRollCounts(rolls) {
+  if (rolls.length === 0){
+    return {};
+  
+}
+}
 // Do not change the code below here.
 module.exports = {
   isValid,
