9555de8f1b15eaba9476d07709f0cf9ff23b2c2b
Abdullah-Tasfiek <atasfiek.ece@gmail.com>
2022-01-31 12:41:43 -0500

    first two functions completed

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  let roll = true;
+  for (let i = 0; i < rolls.length; i++) {
+    if (typeof rolls[i] !== 'number')
+    roll = false;
+  }
+  return roll;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +18,15 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  let found = null;
+  for (let i = 0; i < rolls.length; i++) {
+    if (rolls.includes(value)) {
+      found = value;
+    }
+  }
+  return found;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,7 +34,15 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  // let newArray = [];
+  // for (let i = 0; i < rolls.length; i++) {
+  //   if (rolls[i] >= lowest) {
+  //     newArray.push(rolls[i])
+  //   }
+  // }
+  // return newArray; //new array of numbers
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
