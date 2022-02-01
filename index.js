c264a2e581da8292fca57301ef3825c5a9f37b3a
Jyoti Singh <jyo.singh88@gmail.com>
2022-02-01 04:57:30 -0500

    index tested

--- index.js
+++ index.js
@@ -3,16 +3,64 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+// function isValid(rolls) {
+//   let result = false
+//   if (rolls.length === 0 ){
+//     result = true;
+//   } 
+//   for (const roll of rolls){
+//     console.log(roll.length);
+//      if (typeof roll !== 'number'){
+//     result =false;
+//     }
+//    else {
+// result = true;  
+//   }
+// return result;
+// }
+// }
 
+
+// function isValid(rolls) {
+//   let result = false;
+//   if(rolls.length === 0){
+//     result = true;
+//   }
+//   rolls.forEach(function(element){
+//     if(typeof element === 'number'){
+//       result = true;
+//     }
+//     else {result = false;}
+//   })
+//   return result;
+// }
+
+function isValid(rolls) {
+  let result,
+  rollsLength = rolls.length;
+for(let i=0; i < rollsLength; i++){
+if(typeof rolls[i] === 'number'){
+  result = true;
+}
+else {result = false;}
+}
+return (rolls.length === 0) ? true : result;
+}
+   
+  
+const rolls = [2,4,5,2];
+console.log(isValid(rolls));
+//roll is the element//
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
-
+function findValue(rolls, value) {
+  return (rolls.includes(value)) ? value : null;
+}
+//no need for a for loop here!
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
