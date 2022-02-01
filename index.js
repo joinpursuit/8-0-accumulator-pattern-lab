bff0d1732e9fe2fc41c20ee39d1c75bbe3b499cf
shareeves07 <reeves.shamar1@gmail.com>
2022-02-01 12:11:48 -0500

    test

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  console.log('hey');
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
