d784ae1c9b6b39e6c10ba63ec110d45f6365911f
Matt M <Afrodesiacc@gmail.com>
2022-02-01 11:51:57 -0500

    Finished Lab

--- index.js
+++ index.js
@@ -34,14 +34,39 @@ function findValue(rolls, value) {
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let total = []
+  for (let i = 0; i < rolls.length; i++) {
+    if (rolls[i] >= lowest) {
+    total.push(rolls[i])
+    } else if (rolls.length === 0) {
+      total = []
+    }
+  // returns an array using rolls >= the lowest value
+  }
+  return total
+}
+
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+
+// [10, 12, 30, 45, 12]
+function getRollCounts(rolls) {
+  //loop will use rolls 
+  let obj = {}
+  for (let i = 0; i < rolls.length; i++) {
+  if (obj[rolls[i]]) {
+    obj[rolls[i]] += 1
+  } else {
+    obj[rolls[i]] = 1
+  }
+} 
+  return obj
+}
 
 // Do not change the code below here.
 module.exports = {
