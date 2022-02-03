dc066f4a3c0ea7d7d8c1b3db1fdb599d2ef23720
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-02-03 10:27:06 -0500

    Pass test filterOutLowValues() in index.js.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
  */
  function isValid(rolls) {
   
-
   for (let i = 0; i < rolls.length; i++) {
     if (typeof rolls[i] !== 'number') {
       return false;
@@ -37,14 +36,26 @@ function findValue(rolls, value) {
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let higherValues = [];
+
+  for (let i = 0; i < rolls.length; i++) {
+    if (rolls[i] >= lowest) {
+      higherValues.push(rolls[i]);
+    }
+  }
+  return higherValues;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+
+  
+}
 
 // Do not change the code below here.
 module.exports = {
