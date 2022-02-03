d8bc970f64273d15a1cf2e88b7afc8aac4bc97b5
Tom LaTulipe <tlatulipe@gmail.com>
2022-02-02 21:08:00 -0500

    donezo

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ function isValid(rolls) {
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (let roll of rolls) {
+    if (roll === value) {
+      return value;
+    }
+  }
+  return null;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -26,14 +33,42 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let ofTheMack = []; //the array to be returned
+    for (let roll of rolls){
+      if (roll >= lowest){
+        ofTheMack.push(roll);
+      }
+    }
+    return ofTheMack; //the payoff
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let dasObject = {};
+  let uniqueNums = [];
+
+  for (let roll of rolls){
+    if (!uniqueNums.includes(roll)){
+      uniqueNums.push(roll);
+    }
+  }
+
+  for (let number of uniqueNums){
+    let count = 0;
+    for (let roll of rolls){
+      if (roll == number){
+        count++;
+      }
+    }
+    dasObject[number] = count;
+  }
+  return dasObject;
+}
 
 // Do not change the code below here.
 module.exports = {
