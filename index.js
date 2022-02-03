628fbb84d7a9a8c74ead7c3c82e379adb09ab69a
Mahajaya <mahajaya@Mahajayas-MacBook-Air.local>
2022-02-03 00:07:29 -0500

    Accumulator-Pattern-Lab

--- index.js
+++ index.js
@@ -3,15 +3,40 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+ function isValid(rolls) {
+    for (const roll of rolls) { //roll in this instance = i, of rolls is the length
+    if (typeof(roll) === "number") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
 
+  
+  
+
+  
+
+
+ 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (const index of rolls){
+  if(rolls.includes(value)){
+    return value;
+  }else{
+    return null;
+  }
+}
+  return null;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +44,37 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  newArr = [];
+  for(const index of rolls){
+  if(index >= lowest){
+    newArr.push(index)
+    //return newArr(index);
+    }
+  }
+  return newArr;
+}
+
+
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  newObj = {}
+  for(const roll of rolls){
+    let count = 0;
+    for (let i = 0; i < rolls.length; i++){
+      if (roll === rolls[i]){
+        count++;
+        newObj[roll] = count
+      }
+    }
+  }
+  return newObj;
+}
 
 // Do not change the code below here.
 module.exports = {
