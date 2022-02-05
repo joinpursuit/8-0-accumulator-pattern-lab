4c83eb100f09651eb04f27684a95eab04e59c87f
KenyaMighty <kenyamighty@pursuit.org>
2022-02-05 18:01:02 -0500

    complete changes

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ function isValid(rolls) {
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  let notFound = null;
+  for (i = 0; i < rolls.length; i++) {
+    if (rolls[i] === value) {
+      return value
+    }
+  }
+  return notFound
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -26,14 +34,33 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let newArray = [];
+  for (i = 0; i < rolls.length; i++) {
+    if (rolls[i] >= lowest) {
+      newArray.push(rolls[i])
+    }
+  
+  }
+  return newArray
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let obj = {}
+  for (i = 0; i < rolls.length; i++) {
+    if (obj[rolls[i]]){
+      obj[rolls[i]] += 1
+      } else{
+        obj[rolls[i]] = 1
+      }
+  }
+  return obj
+}
 
 // Do not change the code below here.
 module.exports = {
