6970e9a3f56aa6ba4a4ebf17f7fc0f4b8975c05c
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-06-28 17:09:22 -0400

    All Passing

--- index.js
+++ index.js
@@ -3,30 +3,72 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) { 
+  if (rolls.length === 0){
+    return true
+  }
+  for (i = 0; i < rolls.length; i++){
+    if (typeof(rolls[i]) === 'number'){
+      return true
+    } else {return false}
 
+}
+}
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+let accumulator = 0
+  if (rolls.length === 0){
+    return null
+}
+for (let roll of rolls){
+  if (roll === value){
+      accumulator = roll
+      return accumulator
+  } 
 
+  }
+  return null
+}
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
-
+function filterOutLowValues(rolls, lowest) {
+  let accumulator = []
+  if (rolls.length === 0) {
+    return accumulator
+  }
+  for (let i = 0; i < rolls.length; i++){
+    let roll = rolls[i];
+    if (roll >= lowest){
+        accumulator.push(roll);
+  }
+  }  
+return accumulator
+}
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let accumulator = {}
+  for (let roll of rolls){
+    if (accumulator[roll]){
+      accumulator[roll] += 1
+    } else{
+      accumulator[roll] = 1
+    }
+  }
+  return accumulator
+}
 
 // Do not change the code below here.
 module.exports = {
