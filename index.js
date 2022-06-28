2166e26f97c8b0284ab03d31865225bc42cb7a5c
sreeves07 <shamarreeves@pursuit.org>
2022-06-28 14:21:46 -0400

    seconf problem

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  let accumulator = true;
+  for (let roll of rolls) {
+    if (typeof roll !== 'number') {
+      accumulator = false;
+    }
+  }
+  return accumulator;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +19,14 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (let roll of rolls) {
+    if (roll === value) {
+      return roll;
+    }
+  }
+  return null;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
