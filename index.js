c1a2e5e0b6f825a93a5583c9d30518117175fb54
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2022-06-30 11:28:24 -0400

    Finished the lab

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+ let  accumavariable = true
+ for (let roll of rolls) {
+  if (typeof roll !== "number") {
+    accumavariable = false
+  }
+ }
+ return accumavariable
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +19,15 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+let accuvariable = null
+for(let roll of rolls) {
+  if ( roll === value) {
+     accuvariable = value
+  }
+}
+return accuvariable
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,19 +35,35 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+let accuteaccumulator = []
+for (let roll of rolls) {
+  if (roll >= lowest) {
+    accuteaccumulator.push(roll)
+  }
+}
+return accuteaccumulator
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
-
+function getRollCounts(rolls) {
+let rollbounce = {};
+for (i= 0; i < rolls.length; i++) {
+  rollbounce[rolls[i]] = 0;
+}
+for (let roll of rolls) {
+  rollbounce[roll]++;
+}
+return rollbounce;
+}
 // Do not change the code below here.
 module.exports = {
   isValid,
   findValue,
   filterOutLowValues,
   getRollCounts,
-};
+}
