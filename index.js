e26b3c6b40bb653564acd61c9532850e84b0dc38
youssefmohamedfarouk <youssefmohamedfarouk@pursuit.org>
2022-06-29 21:39:23 -0400

    Add solution to each problem in index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  let allNum = true;
+  for (const roll of rolls) {
+    if (isNaN(roll)) { allNum = false; }
+  }
+  return allNum;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +17,13 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  let foundVal = null;
+  for (const roll of rolls) {
+    if (roll === value) { foundVal = roll; }
+  }
+  return foundVal;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +31,29 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let newArr = [];
+  for (const roll of rolls) {
+    if (roll >= lowest) { newArr.push(roll); }
+  }
+  return newArr;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  const rollNCount = {};
+  for (i = 0; i < rolls.length; i++) {
+    rollNCount[rolls[i]] = 0;
+  }
+  for (const roll of rolls) {
+    rollNCount[roll]++;
+  }
+  return rollNCount;
+}
 
 // Do not change the code below here.
 module.exports = {
