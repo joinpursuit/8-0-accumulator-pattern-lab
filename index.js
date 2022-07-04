683d3140b93a999e4ce7aad998f6db5c1b60813d
bigdaddyjean <jeannappoleon@pursuit.org>
2022-07-04 07:58:35 -0400

    final commit

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+ function isValid(rolls) {
+  for (let roll of rolls) {
+    if (typeof roll !== "number") return false;
+  }
+  return true;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +16,13 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (const roll of rolls) {
+    if (roll === value) return value;
+  }
+
+  return null;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +30,27 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  const result = [];
+  for (const roll of rolls) {
+    if (roll >= lowest) result.push(roll);
+  }
+  return result;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  const result = {};
+  for (const roll of rolls) {
+    if (result[roll]) result[roll]++;
+    else result[roll] = 1;
+  }
+  return result;
+}
 
 // Do not change the code below here.
 module.exports = {
