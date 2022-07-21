c6e167f2dedda12684304d6a7d5eca30bb9e3b39
JorgeM419 <jorgemoran@pursuit.org>
2022-07-21 13:29:02 -0400

    accum done

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+ let allNum = true 
+ for (let roll of rolls){
+  typeof roll!=='number' ? allNum = false : allNum
+  break;
+
+ }
+ return allNum
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +19,13 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+ let newVal = null;
+ for(let roll of rolls){
+  roll === value ? newVal = value : newVal
+ }
+ return newVal
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +33,35 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let bigVal =[]
+  for(let roll of rolls){
+    roll>= lowest ? bigVal.push(roll) : bigVal
+  }
+  return bigVal
+
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let accumalator = {}
+  obj = {}
+  let sum = 0 
+
+  for(let roll of rolls){
+
+    obj[roll] = sum;
+
+  }
+  for(let roll of rolls){
+    obj[roll] += 1
+  }
+  return obj
+}
 
 // Do not change the code below here.
 module.exports = {
