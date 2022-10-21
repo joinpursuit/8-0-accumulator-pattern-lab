50a884a3027b9caf397308644f566542dd38406b
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-10-21 19:03:37 -0400

    Pass filterOutLowValues test

--- index.js
+++ index.js
@@ -39,7 +39,17 @@ function findValue(rolls, value) {
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let filtered = []
+
+  for(let roll of rolls){
+    if(roll >= lowest){
+      filtered.push(roll)
+    }
+  }
+
+  return filtered
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
