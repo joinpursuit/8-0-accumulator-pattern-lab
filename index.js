6d7ed3977b1001dd3179bff8cdea4f1b041d51b3
mrkwab3 <mrkwab3@gmail.com>
2022-10-23 17:50:27 -0400

    Finished Accumulator Pattern Lab

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  for (element of rolls) {
+    if (typeof element !== "number") {
+      return false
+    }
+  }
+  return true
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +18,14 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (element of rolls) {
+    if (element === value) {
+      return element
+    }
+  }
+  return null
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +33,41 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let arr = []
+  for (element of rolls) {
+    if (element >= lowest) {
+      arr.push(element)
+    }
+  }
+  return arr
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  /* FOR VISUALIZATION
+  * rolls = {
+    1: (times rolled)
+    2: (times rolled)
+    ...
+  }
+  */
+  let obj = {}
+
+  for (element of rolls) { //> Initializes each element into our new object
+    obj[element] = 0
+  }
+
+  for (element of rolls) { //> Increments the value of the key every time it is encountered
+    obj[element] += 1
+  }
+
+  return obj
+}
 
 // Do not change the code below here.
 module.exports = {
