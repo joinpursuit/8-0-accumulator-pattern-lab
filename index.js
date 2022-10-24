ee77bb193b858e51140c356244b0b469f180d010
AlexTan21 <87435891+AlexTan21@users.noreply.github.com>
2022-10-24 11:11:55 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  if (rolls.length < 1) {
+    return true
+  }
+  for (let i = 0; i < rolls.length; i++) {
+    return typeof(rolls[i]) === `number`
+  }
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +18,13 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  if (!rolls.includes(value)) {
+    return null
+  } else {
+    return value
+  }
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +32,32 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let numArray = []
+  for (let i = 0; i < rolls.length; i++) {
+    if (rolls[i] >= lowest) {
+      numArray.push(rolls[i])
+    }
+  }
+  return numArray
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let newObject = {}
+  for (let i = 0; i < rolls.length; i++) {
+    if (newObject[rolls[i]]) {
+      newObject[rolls[i]] += 1
+    } else {
+      newObject[rolls[i]] = 1
+    }
+  }
+  return newObject
+}
 
 // Do not change the code below here.
 module.exports = {
