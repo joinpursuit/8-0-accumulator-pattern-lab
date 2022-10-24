eed5061f6e8b91f12e073321bba7776a8c6bcc3e
CarlosP721 <115030924+CarlosP721@users.noreply.github.com>
2022-10-24 19:10:13 -0400

    solved all functions

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  for(let i = 0; i < rolls.length; i++){
+    if(rolls[i] === Number(rolls[i])){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  return true;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +20,14 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for(let i = 0; i < rolls.length; i++){
+    if(rolls[i] === value){
+      return value;
+    }
+  }
+  return null;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +35,32 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let newArr = [];
+  for(let i = 0; i < rolls.length; i++){
+    if(rolls[i] >= lowest){
+      newArr.push(rolls[i]);
+    }
+  }
+  return newArr;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+ function getRollCounts(rolls) {
+  let rollCount = {};
+  for(let i = 0; i < rolls.length; i++){
+    if(rollCount[Number(rolls[i])]){
+      rollCount[Number(rolls[i])] += 1;
+    }else{
+      rollCount[Number(rolls[i])] = 1;;
+    };  
+  }
+  return rollCount;
+}
 
 // Do not change the code below here.
 module.exports = {
