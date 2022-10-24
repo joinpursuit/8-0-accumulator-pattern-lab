e951def6e83056c0753a0cb9062851aada8dee7b
Isaiah Desrosiers <isaiahdesrosiers21@gmail.com>
2022-10-24 13:35:56 -0400

    Satisfy all test cases.

--- index.js
+++ index.js
@@ -3,7 +3,15 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  let result = true;
+  for (let roll of rolls){
+    if (typeof roll !== "number"){
+      return false;
+    }
+  }
+  return result;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +19,15 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  let result = null;
+  for (let roll of rolls){
+    if (value === roll){
+      result = value;
+    }
+  }
+  return result;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +35,33 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let newRolls = []
+  for (let roll of rolls){
+    if (roll >= lowest){
+      newRolls.push(roll);
+    }
+  }
+  return newRolls;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  let counts = {}
+  for (roll of rolls){
+    if (roll in counts){
+      counts[roll] ++;
+    }
+    else{
+      counts[roll] = 1;
+    }
+  }
+  return counts;
+}
 
 // Do not change the code below here.
 module.exports = {
