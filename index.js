dbb87e23e464995ca0cbd947f5c163d328083a78
touhami-ben <brooklyn.ben76@gmail.com>
2022-10-25 09:21:47 -0400

    complete accumulator-pattern-lab

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  
+  return rolls.every(roll => {
+  return typeof roll=== 'number'
+});
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +16,17 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  let foundValue =[0]
+  for (let i = 0 ; i < rolls.length; i ++ ){
+    // const value = rolls[i]
+    if (!rolls.includes(value)){
+        return null;
+    }
+    return value;
+  }
+  return foundValue;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,14 +34,33 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let newArray = []
+  
+  for (let i = 0; i <rolls.length; i++){
+    rolls[i] ===lowest
+  
+    
+  }
+  newArray = rolls.splice(lowest,1)
+  return newArray
+
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  
+
+  let object = {}
+  for (let i = 0 ; i < rolls.length; i++){
+    object[rolls[i]] = (object[rolls[i]] ?? 0)+1
+  }
+  return object;
+}
 
 // Do not change the code below here.
 module.exports = {
