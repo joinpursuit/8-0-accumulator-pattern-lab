b0aecd831dd841cd9fefdf6807119ee5e413eb3c
HaroldF415 <harold.f415@gmail.com>
2022-10-25 01:52:19 -0400

    accumulator pattern lab

--- index.js
+++ index.js
@@ -72,28 +72,28 @@ function getRollCounts(rolls) {
 
   } // ends my forOfLoop
 
-  // return rollObject;
+   return rollObject;
 
   // ! USE THE LOGIC ABOVE AND BELOW TO KEEP TRACK OF DUPLICATEs && || DELETE DUPLICATES. 
   // * Meaning if its not inside and array or inside an object. Throw it in there
   // ! The logic should work when the object or array is empty. And only keep unique values
 
 
-  let rollObject001 = {}   // 1 - 6
-// { 2: 2, 3: 1, 5: 2 }
+//   let rollObject001 = {}   // 1 - 6
+// // { 2: 2, 3: 1, 5: 2 }
 
-  for( let i = 0; i < rolls.length; i++ ){  // rolls[2] => element => 5 // [ 2, 3, 5, 5, 2 ]
+//   for( let i = 0; i < rolls.length; i++ ){  // rolls[2] => element => 5 // [ 2, 3, 5, 5, 2 ]
 
-    if (  !rollObject001[ rolls[i] ] ){ // if you found it .. inside the empty object
-      rollObject[ rolls[i] ] = 1  // if u have found it/.. increment the value held in there by one.
-    }else{
-      rollObject[ rolls[i] ] += 1  // if u havent found. create the key that has the name of the element `rolls[i]` == 2 / initiliaze the value of that key to 1
-    }
+//     if (  !rollObject001[ rolls[i] ] ){ // if you found it .. inside the empty object
+//       rollObject[ rolls[i] ] = 1  // if u have found it/.. increment the value held in there by one.
+//     }else{
+//       rollObject[ rolls[i] ] += 1  // if u havent found. create the key that has the name of the element `rolls[i]` == 2 / initiliaze the value of that key to 1
+//     }
 
-  }
+//   }
 
-  // console.log(rollObject)
-  return rollObject
+//   // console.log(rollObject)
+//   return rollObject
 
 } // ends getRollCounts()
 
